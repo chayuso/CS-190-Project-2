90f5fbb33668863518138402ea78cc89d4132ec0
walshcommaryan <ryw246@gmail.com>
2017-05-09 18:39:42 -0700

    Commit-Triggered Various sounds for childhood memory

--- Assets/StreamingAssets/Audio/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/Wwise_IDs.h
@@ -13,12 +13,23 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID BUBBLES = 769541046U;
+        static const AkUniqueID BUBBLES_STOP = 3315496229U;
+        static const AkUniqueID CLOCK = 1182830809U;
+        static const AkUniqueID CLOCK_STOP = 2828124332U;
         static const AkUniqueID LAUGHING_POINTING = 4193801021U;
         static const AkUniqueID RHODES_PIANO = 2900988856U;
+        static const AkUniqueID RHODES_PIANO_STOP = 3200970231U;
         static const AkUniqueID SEAGULLS_START = 1403647256U;
         static const AkUniqueID SEAGULLS_STOP = 3236452260U;
         static const AkUniqueID SUBMERGE = 1740993339U;
+        static const AkUniqueID SWIMMING = 322811350U;
+        static const AkUniqueID SWIMMING_STOP = 2174459141U;
+        static const AkUniqueID TUNING_FORK = 3063027985U;
         static const AkUniqueID UNDER_WATER = 447521081U;
+        static const AkUniqueID UNDER_WATER_STOP = 15032524U;
+        static const AkUniqueID WARPED_WAVES = 1672957575U;
+        static const AkUniqueID WARPED_WAVES_STOP = 1052006018U;
     } // namespace EVENTS
 
     namespace BANKS
