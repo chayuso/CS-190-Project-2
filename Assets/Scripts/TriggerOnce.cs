f5783796e95b0881a6b2434cb385d3a0fb410b84
Christopher Ayuso <chayuso14@gmail.com>
2017-05-09 19:36:46 -0700

    Trigger Fixes

---
+++ Assets/Scripts/TriggerOnce.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerOnce : MonoBehaviour {
+    public string colenam;
+    void OnTriggerEnter(Collider col)
+    {
+        colenam = col.name;
+        if (colenam == "Base")
+        {
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+}
