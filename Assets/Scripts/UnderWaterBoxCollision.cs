e8be2c3b0da807e62c7c5b87d4ce038700fda302
Christopher Ayuso <chayuso14@gmail.com>
2017-05-10 19:54:09 -0700

    Underwater Footsteps, Dock effects, Jumping,Creaking,Button,Death,Sigh,Blocksmashing, Cannon Block

---
+++ Assets/Scripts/UnderWaterBoxCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnderWaterBoxCollision : MonoBehaviour {
+    public string colname;
+    // Use this for initialization
+    void OnCollisionEnter(Collision col)
+    {
+        colname = col.gameObject.name;
+        if (colname == "Base")
+        {
+            gameObject.GetComponent<CustomTrigger>().Hit();
+        }
+    }
+}
