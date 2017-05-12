b6be5897832c653e66721deea5e8d03ea5c92686
Christopher Ayuso <chayuso14@gmail.com>
2017-05-11 22:23:23 -0700

    AltEnding, Bug Fixes

---
+++ Assets/Scripts/UnderwaterStartTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnderwaterStartTrigger : MonoBehaviour {
+    public string colenam;
+    void OnTriggerEnter(Collider col)
+    {
+        colenam = col.name;
+        if (colenam == "Base")
+        {
+            gameObject.GetComponent<CustomTrigger>().Hit();
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+}
